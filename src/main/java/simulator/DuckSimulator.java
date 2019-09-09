9c414d0eb34b8b70691b44bf2adbe701d685da94
kapop <kapopwsp@gmail.com>
2019-09-09 12:53:18 +0700

    add counter

--- src/main/java/simulator/DuckSimulator.java
+++ src/main/java/simulator/DuckSimulator.java
@@ -8,6 +8,7 @@ import goose.Goose;
 import goose.GooseAdapter;
 import pigeon.Pigeon;
 import pigeon.PigeonAdapter;
+import quack.QuackCounter;
 import quack.Quackable;
 
 public class DuckSimulator {
@@ -16,10 +17,10 @@ public class DuckSimulator {
         simulator.simulate();
     }
     void simulate() {
-        Quackable mallardDuck = new MallardDuck();
-        Quackable redheadDuck = new RedheadDuck();
-        Quackable duckCall = new DuckCall();
-        Quackable rubberDuck = new RubberDuck();
+        Quackable mallardDuck = new QuackCounter(new MallardDuck());
+        Quackable redheadDuck = new QuackCounter(new RedheadDuck());
+        Quackable duckCall = new QuackCounter(new DuckCall());
+        Quackable rubberDuck = new QuackCounter(new RubberDuck());
         Quackable gooseDuck = new GooseAdapter(new Goose());
         Quackable pigeon = new PigeonAdapter(new Pigeon());
         System.out.println("\nDuck Simulator: With Goose Adapter");
@@ -29,6 +30,9 @@ public class DuckSimulator {
         simulate(rubberDuck);
         simulate(gooseDuck);
         simulate(pigeon);
+
+        System.out.println("The ducks quacked " +
+                QuackCounter.getQuacks() + " times");
     }
     void simulate(Quackable duck) {
         duck.quack();
